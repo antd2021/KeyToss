ef1051fa52526fab31d3b563d612586cb51d09c0
antd2021 <144189313+antd2021@users.noreply.github.com>
2025-04-04 09:38:48 -0400

    Models created, mvvm dependancy added

---
+++ KeyToss/Models/Password.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyToss.Models
+{
+    class Password
+    {
+        public int PasswordId { get; set; }
+        public string WebsiteName { get; set; }
+        public string Username { get; set; }
+        public string EncryptedPassword { get; set; }
+        public string UserId { get; set; }
+
+    }
+}
---
+++ KeyToss/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyToss.Models
+{
+    class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string MasterPasswordHash { get; set; } = string.Empty;
+    }
+}
