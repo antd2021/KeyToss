389e72d64dc23624343819f55158f2840794a871
Ethan <Beached@mail.uc.edu>
2025-04-06 17:01:45 -0400

    Finished Basic UI

--- KeyToss/Views/LoginPage.xaml.cs
+++ KeyToss/Views/LoginPage.xaml.cs
@@ -6,4 +6,9 @@ public partial class LoginPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+		Navigation.PushAsync(new SignUpPage());
+    }
 }
