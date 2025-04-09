b93fa49bedf10df3e651a9457d6333f83d71101c
slin05 <97693822+slin05@users.noreply.github.com>
2025-04-09 14:27:17 -0400

    Update App.xaml.cs

--- KeyToss/App.xaml.cs
+++ KeyToss/App.xaml.cs
@@ -1,6 +1,3 @@
-﻿using Microsoft.Maui.Controls;
-using Microsoft.Maui.Graphics;
-
 namespace KeyToss;
 
 public partial class App : Application
@@ -9,20 +6,6 @@ public partial class App : Application
     {
         InitializeComponent();
 
-        var tabbedPage = new TabbedPage();
-
-        tabbedPage.BarBackgroundColor = Color.FromArgb("#6200EE");
-        tabbedPage.BarTextColor = Colors.White;
-        tabbedPage.SelectedTabColor = Colors.White;
-
-        var signUpPage = new SignUpPage();
-        signUpPage.Title = "SIGN UP";
-        tabbedPage.Children.Add(signUpPage);
-
-        var profilePage = new ProfilePage();
-        profilePage.Title = "PROFILE";
-        tabbedPage.Children.Add(profilePage);
-
-        MainPage = tabbedPage;
+        MainPage = new NavigationPage(new Views.LoginPage());
     }
 }
