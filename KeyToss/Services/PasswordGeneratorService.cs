044e1166a166e6dfaa6876e4e53551dee317f61d
antd2021 <144189313+antd2021@users.noreply.github.com>
2025-04-15 23:52:31 -0400

    Completed password generator service

--- KeyToss/Services/PasswordGeneratorService.cs
+++ KeyToss/Services/PasswordGeneratorService.cs
@@ -9,6 +9,21 @@ namespace KeyToss.Services
 {
     internal class PasswordGeneratorService
     {
-       
+        //Generated password is hardcoded at 16 characters and used every charater in the string chars
+        //RandomNumberGenerator is considered a safe way to generate random numbers
+        public string GeneratePassword()
+        {
+            const int passwordLength = 16;
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+            char[] password = new char[passwordLength];
+
+            for (int i = 0; i < passwordLength; i++)
+            {
+                int index = RandomNumberGenerator.GetInt32(chars.Length);
+                password[i] = chars[index];
+            }
+
+            return new string(password);
+        }
     }
 }
