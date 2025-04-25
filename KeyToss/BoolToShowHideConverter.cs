1f4d9ef4a9df4a96f9f74b5ebf6643de1cb8d902
slin05 <97693822+slin05@users.noreply.github.com>
2025-04-25 09:23:56 -0400

    Added Converter File

---
+++ KeyToss/BoolToShowHideConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace KeyToss
+{
+    public class BoolToShowHideConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? "Hide" : "Show";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (string)value == "Hide";
+        }
+    }
+}
